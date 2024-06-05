f82af0f2c452b1378e2e21206d8455edc95b6bf9
sonix <SSonicXX@yandex.ru>
2024-06-05 16:47:15 +0300

    Добавьте файлы проекта.

---
+++ gitTraining/ext.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <map>
+using namespace std;
---
+++ gitTraining/gitTraining(main).cpp
@@ -0,0 +1,6 @@
+﻿#include "ext.h"
+
+int main()
+{
+    return 0;
+}
