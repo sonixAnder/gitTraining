7e2590db4310a8a267643c4df9cb793d56ba7f9d
sonix <SSonicXX@yandex.ru>
2024-06-05 17:49:49 +0300

    удалена функция вывода всех параметров структуры человека

--- gitTraining/PrintPars.cpp
+++ gitTraining/PrintPars.cpp
@@ -1,11 +1,5 @@
 #include "PrintPars.h"
 
-void printPersonPars(Person p)
-{
-	cout << "ÔÈÎ: " << p.name << " " << p.secondName << " " << p.surname << "." << endl;
-	cout << "Âîçðàñò: " << p.age << endl;
-}
-
 void printPersonFIO(Person p)
 {
 	cout << "ÔÈÎ: " << p.name << " " << p.secondName << " " << p.surname << "." << endl;
